4ece9003d3841a24629c99f0ddaa63225b9dd156
LeskovarLukas <leskovar.lukas@student.htlwrn.ac.at>
2021-09-23 18:18:25 +0200

    refactor: set program to sleep 0.5s within loop

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -2,6 +2,23 @@
 // Created by Lukas Leskovar on 20.09.21.
 //
 
+#include <iostream>
+#include <unistd.h>
+
 int main() {
+    pid_t pid{fork()};
+
+    if (pid == 0) {
+        do {
+            std::cout << "B" << std::flush;
+            usleep(500000);
+        } while (true);
+    } else {
+        do {
+            std::cout << "A" << std::flush;
+            usleep(500000);
+        } while (true);
+    }
+
     return 0;
 }

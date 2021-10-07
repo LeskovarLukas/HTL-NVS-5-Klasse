680570a990fa229e80232985686a58870a2a82b7
LeskovarLukas <leskovar.lukas@student.htlwrn.ac.at>
2021-10-07 16:52:47 +0200

    adding random lap time

--- 02_threads/src/Car.cpp
+++ 02_threads/src/Car.cpp
@@ -5,15 +5,25 @@
 #include <iostream>
 #include <thread>
 #include <utility>
+#include <random>
+
 #include "include/Car.h"
 
 void Car::driveLaps() {
     using namespace std::literals;
+
     int lapCount{};
+    std::random_device rd;
+    std::mt19937 gen{rd()};
+    std::uniform_real_distribution<> dis{1, 10};
+    std::string output;
+    int waitTime;
+
     while (true) {
         std::string output = std::to_string(++lapCount) + " " + this->carType + "\n";
-        std::this_thread::sleep_for(1s);
-        std::cout << output <<std::flush;
+        waitTime = dis(gen) * 1000;
+        std::this_thread::sleep_for(std::chrono::milliseconds{waitTime});
+        std::cout << output << std::flush;
     }
 }
 
--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <thread>
+#include <random>
 
 #include "Car.h"
 
@@ -11,7 +12,7 @@ int main() {
     std::thread car1{hyundai_i10};
 
     Car volvo = Car("Volvo V40");
-    std::thread car2{volvo};
+    //std::thread car2{volvo};
 
     car1.join();
     return 0;
@@ -20,11 +21,18 @@ int main() {
 
 void hyundai_i10() {
     using namespace std::literals;
+
+    std::random_device rd;
+    std::mt19937 gen{rd()};
+    std::uniform_real_distribution<> dis{1, 10};
+    std::string output;
+    int waitTime;
+
     int lapCount{};
     while (true) {
-        std::string output = std::to_string(++lapCount) + " Hyundai i10\n";
-
-        std::this_thread::sleep_for(1s);
+        output = std::to_string(++lapCount) + " Hyundai i10\n";
+        waitTime = dis(gen) * 1000;
+        std::this_thread::sleep_for(std::chrono::milliseconds{waitTime});
         std::cout << output << std::flush;
     }
 }

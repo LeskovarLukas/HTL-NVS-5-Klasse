55ce1396db39d8dae78dac36fa39f43554f0dfde
LeskovarLukas <leskovar.lukas@student.htlwrn.ac.at>
2021-10-19 23:20:08 +0200

    adding threads

--- 03_sync1/src/main.cpp
+++ 03_sync1/src/main.cpp
@@ -8,14 +8,28 @@ using namespace std;
 int main() {
     Account account;
 
-    account.deposit(15);
-    std::cout << account.getBalance() << std::endl;
+    //Punkt 1
+//    account.deposit(15);
+//    std::cout << account.getBalance() << std::endl;
+//
+//    account.withdraw(6);
+//    std::cout << account.getBalance() << std::endl;
+//
+//    std::cout << account.withdraw(10) << std::endl;
+//    std::cout << account.getBalance() << std::endl;
 
-    account.withdraw(6);
-    std::cout << account.getBalance() << std::endl;
+    account.deposit(1);
 
-    std::cout << account.withdraw(10) << std::endl;
-    std::cout << account.getBalance() << std::endl;
+    thread t1{[&]() {
+        std::cout << account.withdraw(1) << std::endl;
+    }};
+
+    thread t2{[&]() {
+        std::cout << account.withdraw(1) << std::endl;
+    }};
+
+    t1.join();
+    t2.join();
 
     return 0;
 }

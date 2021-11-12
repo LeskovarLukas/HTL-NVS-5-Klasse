01854db6928b22c9b89e26c3f698d223644bf120
LeskovarLukas <leskovar.lukas@student.htlwrn.ac.at>
2021-11-12 18:08:01 +0100

    adding worker threads

--- 04_sync2/src/main.cpp
+++ 04_sync2/src/main.cpp
@@ -2,9 +2,15 @@
 #include <iostream>
 #include <thread>
 
+void worker(int, WorkQueue&);
+
+
 int main() {
     WorkQueue queue = WorkQueue();
     int counter = 0;
+
+    std::thread t1(worker, 1, std::ref(queue));
+    std::thread t2(worker, 2, std::ref(queue));
 
     while (true)
     {
@@ -16,4 +22,14 @@ int main() {
     
 
     return 0;
+}
+
+
+void worker(int id, WorkQueue& q) {
+    while(true) {
+        std::cout << "W" << id << ": want work packet" << std::endl;
+        WorkPacket p = q.pop();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "W" << id << ": Processed work packet " << p.getID() << std::endl;
+    }
 }

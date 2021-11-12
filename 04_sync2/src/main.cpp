f85a0b2c8cf9f6709cdd7f9f10d10542923eee6b
LeskovarLukas <leskovar.lukas@student.htlwrn.ac.at>
2021-11-12 20:06:03 +0100

    adding random wait time for boss and workers

--- 04_sync2/src/main.cpp
+++ 04_sync2/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <thread>
 #include <sstream>
+#include <random>
+#include <iomanip>
 #include "../include/work_queue.h"
 
 void worker(int, WorkQueue&);
@@ -9,21 +11,25 @@ void worker(int, WorkQueue&);
 int main() {
     WorkQueue queue{};
     int counter = 0;
-    std::ostringstream buffer;
+    std::random_device rd;
+    std::mt19937 gen{rd()};
+    std::uniform_real_distribution<> dis{0, 1};
 
     std::thread t1(worker, 1, std::ref(queue));
     std::thread t2(worker, 2, std::ref(queue));
+    std::thread t3(worker, 3, std::ref(queue));
+
+    std::cout << std::setprecision(3);
 
     while (true)
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        int waitTime = dis(gen) * 1000;
+        std::this_thread::sleep_for(std::chrono::milliseconds{waitTime});
 
         std::lock_guard<std::mutex> lock(queue.queue_mutex);
         queue.push(WorkPacket(counter)); 
 
-        buffer << "B: Submitted work packet " << counter++ << std::endl;
-        std::cout << buffer.str() << std::flush;
-        buffer.str("");
+        std::cout << "B: Submitted work packet " << counter++ << " (" << waitTime / 1000.0 << "s)" << std::endl;
 
         queue.not_empty.notify_one();
     }
@@ -37,6 +43,11 @@ int main() {
 
 void worker(int id, WorkQueue& q) {
     std::ostringstream buffer;
+    std::random_device rd;
+    std::mt19937 gen{rd()};
+    std::uniform_real_distribution<> dis{1, 10};
+
+    buffer << std::setprecision(3);
 
     while(true) {
         buffer << "W" << id << ": Want work packet" << std::endl;
@@ -47,16 +58,14 @@ void worker(int id, WorkQueue& q) {
         q.not_empty.wait(lock);
 
         WorkPacket p = q.pop();
-
-        buffer << "W" << id << ": Got work packet " << p.getID() << std::endl;
-        std::cout << buffer.str() << std::flush;
-        buffer.str("");
+        std::cout << "W" << id << ": Got work packet " << p.getID() << std::endl;
 
         lock.unlock();
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        int waitTime = dis(gen) * 1000;
+        std::this_thread::sleep_for(std::chrono::milliseconds{waitTime});
 
-        buffer << "W" << id << ": Processed work packet " << p.getID() << std::endl;
+        buffer << "W" << id << ": Processed work packet " << p.getID() << " (" << waitTime / 1000.0 << "s)" << std::endl;
         std::cout << buffer.str() << std::flush;
         buffer.str("");
     }
